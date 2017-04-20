61f08a794c459d6139549d2206e0134e570e16b7
Bash Lab <tai@bash.vn>
2017-04-21 00:43:59 +0700

    Apply header, footer for all pages, navigate lookout page

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <div class="container">
+      <el-menu mode="horizontal" class="header__menu">
+        <div class="left-navbar">
+          <el-menu-item class="logo" index="1"><img src="../../assets/logo.png" width="250"/></el-menu-item>
+          <el-menu-item class="look-out"index="2">Lookout</el-menu-item>
+        </div>
+        <div class="right-navbar">
+          <el-submenu index="3">
+            <template slot="title">Products</template>
+            <el-menu-item index="3-1">item one</el-menu-item>
+            <el-menu-item index="3-2">item two</el-menu-item>
+            <el-menu-item index="3-3">item three</el-menu-item>
+          </el-submenu>
+          <el-menu-item index="4">About</el-menu-item>
+          <el-menu-item index="4">Hire us</el-menu-item>
+          <el-menu-item index="4">Contact us</el-menu-item>
+        </div>
+      </el-menu>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header-comp',
+};
+</script>
+
--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import Element from 'element-ui';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'element-ui/lib/theme-default/index.css';
 import App from './App';
 import router from './router';
 
