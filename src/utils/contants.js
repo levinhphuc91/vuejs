61fb42c82365b6b311c26533e1460549ed261f5d
Phuc Le <levinhphuc91@gmail.com>
2017-05-13 23:47:55 +0800

    make it better

---
+++ src/utils/contants.js
@@ -0,0 +1,14 @@
+const LIST_PLATFORM = [
+  'facebook',
+  'twitter',
+  'wechat',
+  'whatsapp',
+  'line',
+  'line_timeline',
+  'slack',
+  'skype',
+  'imessage',
+  'telegram',
+];
+
+export default LIST_PLATFORM;
